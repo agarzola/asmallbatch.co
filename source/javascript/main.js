7e1c12f939242b0c9481cf8e63c0a5b64b91f613
Alfonso Gómez-Arzola <alfonso@asmallbatch.co>
2016-07-29 16:01:53 -0400

    Track scaffolding with source

---
+++ source/javascript/main.js
@@ -0,0 +1,6 @@
+// Single module file
+(function init_some_specific_behavior () {
+  // Keep it focused. Create other files like this one
+  // to handle other matters. These will be concatenated
+  // and uglified into one `all.js` file.
+})();
