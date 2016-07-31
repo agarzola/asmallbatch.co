177adf3fe1b330558ccf90ffbb44c94cc7719fc4
Alfonso Gómez-Arzola <alfonso@asmallbatch.co>
2016-07-30 21:28:09 -0400

    Re-enhance footer

--- source/javascript/modules/enhanced_view.js
+++ source/javascript/modules/enhanced_view.js
@@ -4,20 +4,36 @@
   var footer = document.getElementById('footer');
 
   document.body.className += ' enhanced';
+  center_content(header);
   set_main_margin();
-  set_header_padding();
+  center_content(footer);
 
+  window.addEventListener('resize', function () {
+    center_content(header);
+  });
   window.addEventListener('resize', set_main_margin);
-  window.addEventListener('resize', set_header_padding);
+  window.addEventListener('resize', function () {
+    center_content(footer);
+  });
+  window.addEventListener('scroll', set_footer_visibility);
 
   function set_main_margin () {
     main.style.marginTop = window.innerHeight + 'px';
+    main.style.marginBottom = window.innerHeight + 'px';
   }
 
-  function set_header_padding () {
-    var container = header.getElementsByClassName('container')[0];
-    header.style.paddingTop =
-      ((header.clientHeight - container.clientHeight) / 2)
+  function center_content (el) {
+    var container = el.getElementsByClassName('container')[0];
+    el.style.paddingTop =
+      ((el.clientHeight - container.clientHeight) / 2)
       + 'px';
+  }
+
+  function set_footer_visibility () {
+    if (window.scrollY > window.innerHeight) {
+      footer.style.zIndex = 1;
+    } else {
+      footer.style.zIndex = 0;
+    }
   }
 })();
