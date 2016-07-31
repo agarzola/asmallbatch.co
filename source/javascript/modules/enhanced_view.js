114db8f8b79776ced038dd599e249a7232cc4fe1
Alfonso Gómez-Arzola <alfonso@asmallbatch.co>
2016-07-30 21:05:02 -0400

    Fix event listener; add event listener

--- source/javascript/modules/enhanced_view.js
+++ source/javascript/modules/enhanced_view.js
@@ -3,11 +3,12 @@
   var main = document.getElementById('main');
   var footer = document.getElementById('footer');
 
-  window.addEventListener('scroll', set_main_margin);
-
   document.body.className += ' enhanced';
   set_main_margin();
   set_header_padding();
+
+  window.addEventListener('resize', set_main_margin);
+  window.addEventListener('resize', set_header_padding);
 
   function set_main_margin () {
     main.style.marginTop = window.innerHeight + 'px';
@@ -15,7 +16,8 @@
 
   function set_header_padding () {
     var container = header.getElementsByClassName('container')[0];
-    console.log(header.clientHeight, container.clientHeight, ((header.clientHeight - container.clientHeight) / 2))
-    header.style.paddingTop = ((header.clientHeight - container.clientHeight) / 2) + 'px';
+    header.style.paddingTop =
+      ((header.clientHeight - container.clientHeight) / 2)
+      + 'px';
   }
 })();
