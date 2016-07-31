8704e5fd3f2dff389490d7bb68b0aab81244f9f2
Alfonso Gómez-Arzola <alfonso@asmallbatch.co>
2016-07-30 21:01:50 -0400

    Veritcally center header content when enhanced

--- source/javascript/modules/enhanced_view.js
+++ source/javascript/modules/enhanced_view.js
@@ -1,13 +1,21 @@
 (function enhanced_view () {
-  var footer = document.getElementById('footer');
+  var header = document.getElementById('header');
   var main = document.getElementById('main');
+  var footer = document.getElementById('footer');
+
+  window.addEventListener('scroll', set_main_margin);
+
   document.body.className += ' enhanced';
+  set_main_margin();
+  set_header_padding();
 
-  window.addEventListener('scroll', determine_main_margin);
-
-  determine_main_margin();
-
-  function determine_main_margin () {
+  function set_main_margin () {
     main.style.marginTop = window.innerHeight + 'px';
+  }
+
+  function set_header_padding () {
+    var container = header.getElementsByClassName('container')[0];
+    console.log(header.clientHeight, container.clientHeight, ((header.clientHeight - container.clientHeight) / 2))
+    header.style.paddingTop = ((header.clientHeight - container.clientHeight) / 2) + 'px';
   }
 })();
