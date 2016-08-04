ae704420066797f6825e734b42323630ca39b34b
Alfonso Gómez-Arzola <alfonso@asmallbatch.co>
2016-08-04 14:56:43 -0400

    Fix click/focus race condition

--- source/javascript/modules/scrolling.js
+++ source/javascript/modules/scrolling.js
@@ -3,16 +3,23 @@
 
   var header = document.getElementById('header');
   var anchor_link = header.getElementsByTagName('a')[0];
+  var footer = document.getElementById('footer');
+  var email = footer.getElementsByTagName('a')[0];
+  var footer_target = document.getElementById('tell-us-about-your-project');
 
   anchor_link.addEventListener('focus', function (event) {
     if (document.body.className.match(/enhanced/)) {
       window.zenscroll.toY(0);
     }
-  })
+  });
 
-  var footer = document.getElementById('footer');
-  var email = footer.getElementsByTagName('a')[0];
-  var footer_target = document.getElementById('tell-us-about-your-project');
+  anchor_link.addEventListener('mousedown', function (event) {
+    if (document.body.className.match(/enhanced/)) {
+      event.stopImmediatePropagation();
+      event.preventDefault();
+      window.zenscroll.to(footer_target);
+    }
+  });
 
   email.addEventListener('focus', function (event) {
     if (document.body.className.match(/enhanced/)) {
