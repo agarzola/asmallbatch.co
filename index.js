0e6757f3ce927467b2e89bbee17922657bc19e7b
Alfonso Gómez-Arzola <alfonso@asmallbatch.co>
2016-08-06 00:01:23 -0400

    Set content-type according to extension

--- index.js
+++ index.js
@@ -59,6 +59,10 @@ function serve_assets(req, res) {
   var file_path = __dirname + '/assets' + req.url_parts.pathname
   fs.stat(file_path, (err, stat) => {
     if (err !== null) { return serve_error(req, res, 404, 'File not found') }
+    var extension = file_path.match(/\.(.{2,3})$/)[1]
+    res.writeHead(200, 'OK', {
+      'Content-Type': extension === 'js' ? 'application/javascript' : extension === 'css' ? 'text/css' : 'image/' + extension
+    })
     var file_stream = fs.createReadStream(file_path)
     file_stream.on('open', () => {
       file_stream.pipe(res)
