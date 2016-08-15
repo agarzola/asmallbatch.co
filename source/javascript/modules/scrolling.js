fbc4be8d916164a33de4d97eeefec16a082f2d54
Alfonso Gómez-Arzola <alfonso@asmallbatch.co>
2016-08-15 19:45:25 -0400

    Apply what was previously email link event handler to first input

--- source/javascript/modules/scrolling.js
+++ source/javascript/modules/scrolling.js
@@ -4,7 +4,7 @@
   var header = document.getElementById('header');
   var anchor_link = header.getElementsByTagName('a')[0];
   var footer = document.getElementById('footer');
-  var email = footer.getElementsByTagName('a')[0];
+  var first_field = footer.getElementsByTagName('input')[0];
   var footer_target = document.getElementById('tell-us-about-your-project');
 
   anchor_link.addEventListener('focus', function (event) {
@@ -21,7 +21,7 @@
     }
   });
 
-  email.addEventListener('focus', function (event) {
+  first_field.addEventListener('focus', function (event) {
     if (document.body.className.match(/enhanced/)) {
       window.zenscroll.to(footer_target);
     }
